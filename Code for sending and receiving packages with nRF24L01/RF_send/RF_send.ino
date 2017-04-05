25d120d2318a569af4d759e93c6ced12721a713b
Gil Leibovitz <leibovitzgil@gmail.com>
2017-04-05 13:44:40 +0300

    added Code for sending Packages via nRF24L01 Sensor Sample

---
+++ Code for sending and receiving packages with nRF24L01/RF_send/RF_send.ino
@@ -0,0 +1,47 @@
+#include <SPI.h>  
+#include "RF24.h"
+
+RF24 myRadio (9, 10);
+byte addresses[][6] = {"0"};
+
+struct package
+{
+  int id=1;
+  float temperature = 18.3;
+  char  text[100] = "Text to be transmitted";
+};
+
+
+typedef struct package Package;
+Package data;
+
+
+void setup()
+{
+  Serial.begin(115200);
+  myRadio.begin();  
+  myRadio.setChannel(115); 
+  myRadio.setPALevel(RF24_PA_MIN);
+  myRadio.setDataRate( RF24_250KBPS ) ; 
+  myRadio.openWritingPipe( addresses[0]);
+}
+
+void loop()
+{
+  myRadio.write(&data, sizeof(data)); 
+
+  Serial.print("\nPackage:");
+  Serial.print(data.id);
+  Serial.print("\n");
+  Serial.println(data.temperature);
+  Serial.println(data.text);
+  data.id = data.id + 1;
+  data.temperature = data.temperature+0.1;
+  
+//  int ts=micros();
+//  while(micros()-ts>10000){
+//    Serial.print("Reaching Loop");
+//  }
+  
+
+}
